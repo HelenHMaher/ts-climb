87efb15d5dbdf277e3a1bdda8f9968b6960087c8
Helen_Maher <helenhmaher@gmail.com>
2021-01-12 21:03:37 +0100

    heroku deploy

--- server/index.js
+++ server/index.js
@@ -37,12 +37,12 @@ app.use(
 );
 
 // TODO: this is for production, not required for development
-// add : " "proxy": "http://localhost:3001", " to package.json
-// app.use('/', express.static(path.join(__dirname, '../build')));
+// remove : " "proxy": "http://localhost:3001", " to package.json
+app.use('/', express.static(path.join(__dirname, '../build')));
 
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, '../build', 'index.html'));
-// });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 //MIDDLEWARE
 
