83476f13a4bbec6b8bf22e660dd5adf211be0dd9
Helen_Maher <helenhmaher@gmail.com>
2021-01-12 21:21:10 +0100

    heroku

--- server/index.js
+++ server/index.js
@@ -38,11 +38,11 @@ app.use(
 
 // TODO: this is for production, not required for development
 // remove : " "proxy": "http://localhost:3001", " to package.json
-// app.use('/', express.static(path.join(__dirname, '../build')));
+app.use('/', express.static(path.join(__dirname, '../build')));
 
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, '../build', 'index.html'));
-// });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 //MIDDLEWARE
 
